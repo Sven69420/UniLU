d0d678f68ae1978ed251b1aea25ab07893be540d
Sven <53235203+Sven69420@users.noreply.github.com>
2023-11-07 16:40:04 +0100

    Delete Programming Fundamentals directory

--- Programming Fundamentals/MagicNumberSeries.py
+++
@@ -1,7 +0,0 @@
-def is_magic_sequence(s):
-    """checks to see if the integers read the same forwards and backwards"""
-    if abs(s) == abs(s)[::-1]:
-        return True
-    else:
-        return False
-
--- Programming Fundamentals/RecursiveSumSeries.py
+++
@@ -1,8 +0,0 @@
-def sum_series(n):
-    """Function that gives back the sum of the series from 1 to n."""
-    if n == 0:
-        return 0
-    else:
-        return n + sum_series(n - 1)
-
-print(sum_series(5))
